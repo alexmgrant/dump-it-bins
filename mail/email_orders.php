fd9901a4f51d570497435d3ef0c6ac9bd9425d96
Alex Grant <alexmgrant@me.com>
2016-12-16 14:13:01 -0500

    Adding designs and logos

--- mail/email_orders.php
+++ mail/email_orders.php
@@ -28,7 +28,7 @@ $datePicker = strip_tags(htmlspecialchars($_POST['datePicker']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'info@dumpitbins.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@dumpitbins.com'; // This is where the form will send a message to.
 $email_subject = "**ORDER** from:  $name";
 $email_body = "You have received a new order from dumpitbins.com.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nAddress: $address1\n\nAdress: $address2\n\nCity: $city\n\nProvince $province\n\nPostalCode: $postalCode\n\nBin Size: $binSize\n\nDelivery: $datePicker\n\nMessage:\n$message";
 $headers = "From: noreply@dumpitbins.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
